4eb1dbb7e5c317fb82c906ea7b8d227e60cca55c
unknown <m.siegrist92@gmail.com>
2020-09-24 20:42:38 -0400

    basic styles for navbar and icon

--- src/routers/pages.js
+++ src/routers/pages.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const router = new express.Router();
 
+router.get('/', async (req, res) => {
+  await res.render('create', {
+    option_1: "Log In",
+    option_2: "Sign Up",
+    option_3: "Help",
+    option_4: "About",
+  });
+})
+
 router.get('/create', async (req, res) => {
-  await res.render('create');
+  await res.render('create', {
+    option_1: "Create",
+    option_2: "Calendar",
+    option_3: "Recents",
+    option_4: "Search",
+    option_5: "Log Out"
+  });
 })
 
 router.get('/recent', async (req, res) => {
-  await res.render('recent');
+  await res.render('recent', {
+    option_1: "Create",
+    option_2: "Calendar",
+    option_3: "Recents",
+    option_4: "Search",
+    option_4: "Log Out"
+  });
 })
 
 router.get('/calendar', async (req, res) => {
-  res.render('calendar');
+  res.render('calendar', {
+    option_1: "Create",
+    option_2: "Calendar",
+    option_3: "Recents",
+    option_4: "Search",
+    option_4: "Log Out"
+  });
 })
 
 module.exports = router;
