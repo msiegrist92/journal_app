591399217e682557c5d3bd8b67eff5062e059e8c
Mark <m.siegrist92@gmail.com>
2020-10-28 10:53:59 -0400

    arrows added, responsive styles, event controllers for navigation

---
+++ public/js/calendar_nav.js
@@ -0,0 +1,44 @@
+const back = document.getElementById('back_arrow');
+const forward = document.getElementById('forward_arrow');
+
+const monthURL = /(?<=\/)\d+(?=&)/g;
+const yearURL = /(?<=&)\d{4}/g;
+
+
+const changeMonth = (direction) => {
+  const current_URL = window.location.href;
+  let current_month = parseInt(current_URL.match(monthURL));
+  let current_year = parseInt(current_URL.match(yearURL));
+
+  if(direction === 'forward'){
+    if(current_month === 11){
+      current_month = 0;
+      current_year += 1;
+    } else {
+      current_month += 1;
+    }
+  }
+
+  if(direction === 'backward') {
+      if(current_month === 0){
+        current_month = 11;
+        current_year -= 1;
+      } else {
+        current_month -= 1;
+      }
+  }
+
+  const redirect = '/calendar/' + current_month + '&' + current_year;
+  window.location = redirect;
+
+}
+
+back.addEventListener('click', (e) => {
+  e.preventDefault();
+  changeMonth('backward');
+})
+
+forward.addEventListener('click', (e) => {
+  e.preventDefault();
+  changeMonth('forward');
+})
